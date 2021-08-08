b323cf11ba2d58080c35a37c5d3b5bccb0e4adf7
falconlee236 <falconlee236@naver.com>
2021-08-08 03:47:56 +0000

    Add: Solve 1676

---
+++ BaekJoon/1676.cpp
@@ -0,0 +1,20 @@
+/*1676*/
+/*cheating*?
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n; scanf("%d", &n);
+    int res = 0, mul = 1;
+    for(int i = 1; i <= n; i++){
+        mul *= i;
+        
+        while(mul % 10 == 0){
+            mul /= 10;
+            res++; 
+        }
+        mul %= 1000;
+    }
+    printf("%d", res);
+    return 0;
+}
