4bf2f03e3f248e995f1fa140fb248ab78ef859bd
falconlee236 <falconlee236@naver.com>
2021-08-21 00:09:42 +0000

    Add: Solve 1759

--- BaekJoon/1759.cpp
+++ BaekJoon/1759.cpp
@@ -1,23 +1,33 @@
+/*1759*/
+/*Cheating*/
 #include <iostream>
-#include <vector>
+#include <string>
 #include <algorithm>
 using namespace std;
 
+int l, c;
+char alpha[16];
+
+void func(int pos, string str, int vowel, int cons){
+    if(str.size() == l){
+        if(vowel < 1 || cons < 2) return;
+        cout << str << "\n";
+        return;
+    }
+    
+    for(int i = pos; i < c; i++){
+        char ch = alpha[i];
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            func(i + 1, str + ch, vowel + 1, cons);
+        else 
+            func(i + 1, str + ch, vowel, cons + 1);
+    }
+}
+
 int main(){
-    int l, c; cin >> l >> c;
-    vector<char> alpha;
-    int cons_size = 0, vowel_size = 0;
-    for(int i = 0; i < c; i++){
-        char ch; scanf("%c", &ch);
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') vowel_size++;
-        else cons_size++;
-        alpha.push_back(ch);
-    }
-    sort(alpha.begin(), alpha.end());
-    
-    for(int i = 1; i <= vowel_size; i++){
-        if(cons_size - i < 2) continue;
-        
-    }
-    
+    cin >> l >> c;
+    for(int i = 0; i < c; i++) cin >> alpha[i];
+    sort(alpha, alpha + c);
+    func(0, "", 0, 0);
+    return 0;
 }
