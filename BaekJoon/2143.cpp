1bd0a86c61d261f3235a5cf1fa1c4c1961b98ee9
falconlee236 <falconlee236@naver.com>
2021-08-24 13:13:27 +0000

    Add: Solve 2143

---
+++ BaekJoon/2143.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int a[1001], b[1001];
+	int t; cin >> t;
+	int n; cin >> n;
+	for(int i = 0; i < n; i++) cin >> a[i];
+	int m; cin >> m;
+	for(int i = 0; i < m; i++) cin >> b[i];
+	
+	vector<int> u, v;
+	for(int i = 0; i < n; i++){
+	    int tmp = a[i];
+	    u.push_back(tmp);
+	    for(int j = i + 1; j < n; j++){
+	        tmp += a[j];
+	        u.push_back(tmp);
+	    }
+	}
+	
+	for(int i = 0; i < m; i++){
+	    int tmp = b[i];
+	    v.push_back(tmp);
+	    for(int j = i + 1; j < m; j++){
+	        tmp += b[j];
+	        v.push_back(tmp);
+	    }
+	}
+	sort(v.begin(), v.end());
+	sort(u.begin(), u.end());
+	
+	long long ans = 0;
+	for(auto i = 0; i < u.size(); i++){
+	    ans += (upper_bound(v.begin(), v.end(), t - u[i]) - lower_bound(v.begin(), v.end(), t - u[i]));
+	}
+	cout << ans;
+	return 0;
+}
