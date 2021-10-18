c9a7583bc8adff958dfb0fc9fb0fe158ed5d4b0e
falconlee236 <falconlee236@naver.com>
2021-10-18 20:30:42 +0900

    Add: Solve 9237

---
+++ BaekJoon/9237.cpp
@@ -0,0 +1,21 @@
+/*9237*/
+/*Cheating*/
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n; scanf("%d", &n);
+	int arr[n];
+	for(int i = 0; i < n; i++){
+	    scanf("%d", &arr[i]);
+	}
+	sort(arr, arr + n, greater<int>());
+	int ans = 0, idx = 2;
+	for(int i = 0; i < n; i++){
+	    ans = max(ans, idx + arr[i]);
+	    idx++;
+	}
+	printf("%d", ans);
+	return 0;
+}
