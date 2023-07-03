91ec626b27eaacb2701d8356f3d54ba4b0c5e66a
falconlee236 <falconlee236@cau.ac.kr>
2023-07-04 01:30:03 +0900

    not solved abc 308 c problem

---
+++ atcoder/ABC_308/C-Standings.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(void){
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    int n; cin >> n;
+    vector<pair<double, int>> ans;
+    for(int i = 0; i < n; i++){
+        int a, b; cin >> a >> b;
+        ans.emplace_back((double)a / (a + b), i + 1);
+    }
+    sort(ans.begin(), ans.end(),
+    [](pair<double, int> &a, pair<double, int> &b) -> bool
+    {
+        if (a.first == b.first) return false;
+        return a.first > b.first;
+    });
+    for(int i = 0; i < n; i++) cout << ans[i].second << " ";
+}
