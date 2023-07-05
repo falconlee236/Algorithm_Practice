3f2e2e4be25b97a399b7cf09f1507f01c838db1f
falconlee236 <falconlee236@cau.ac.kr>
2023-07-06 01:51:37 +0900

    check this problem abc 308

--- atcoder/ABC_308/C-Standings.cpp
+++ atcoder/ABC_308/C-Standings.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 int main(void){
     ios_base::sync_with_stdio(false); cin.tie(0);
-    int n; cin >> n;
-    vector<pair<double, int>> ans; 
-    for(int i = 0; i < n; i++){
-        int a, b; cin >> a >> b;
-        ans.emplace_back((double)a / (a + b), i + 1);
-    }
-    sort(ans.begin(), ans.end(),
-    [](pair<double, int> &a, pair<double, int> &b) -> bool
+    int n;
+    cin >> n;
+    vector<pair<int, int>> ab;
+    for (int i = 0; i < n; i++)
     {
-        if (a.first == b.first) return false;
-        return a.first > b.first;
-    });
-    for(int i = 0; i < n; i++) cout << ans[i].second << " ";
+        int a, b;
+        cin >> a >> b;
+        ab.emplace_back(a, a + b);
+    }
+    vector<int> p(n);
+    iota(p.begin(), p.end(), 0);
+    auto f = [&](int i, int j)
+    {
+        auto [ai, aj] = ab[i];
+        auto [bi, bj] = ab[j];
+        return (long long)ai * bj > (long long)aj * bi;
+    };
+    stable_sort(p.begin(), p.end(), f);
+    for (int i = 0; i < n; i++)
+        cout << p[i] + 1 << " \n"[i == n - 1];
 }
