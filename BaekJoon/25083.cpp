e0e58ffc5fcafeac4393c7e0e61dee418b1b59bf
falconlee236 <falconlee236@cau.ac.kr>
2023-09-09 13:08:07 +0900

    solved 25083

---
+++ BaekJoon/25083.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    cout << "         ,r\'\"7" << "\n";
+    cout << "r`-_   ,'  ,/" << "\n";
+    cout << " \\. \". L_r\'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+}
