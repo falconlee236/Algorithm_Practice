f30ae8d3799dba82d58db06757936c06e6ab3018
falconlee236 <falconlee236@cau.ac.kr>
2024-06-23 20:00:50 +0900

    revise code

--- BaekJoon/25400.cpp
+++ BaekJoon/25400.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    int n; cin >> n;
-    int cnt = 1, res = 0;
+    int n, cnt = 1, res = 0; cin >> n;
     for(int i = 0; i < n; i++){
         int x; cin >> x;
         if (x != cnt) res++;
