7b2ab4b109828dc5ece8681071af2322e749cc34
falconlee236 <falconlee236@cau.ac.kr>
2024-10-15 10:59:14 +0900

    second solve 25634

--- BaekJoon/25634.cpp
+++ BaekJoon/25634.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-int dp[200001];
 int main(){
 	cin.tie(0)->sync_with_stdio(0);
 	int n; cin >> n;
-	int a[n], ans = 0, state;
+	int a[n], state, ans = 0;
 	for(int &x : a) cin >> x;
-	for(int i = 1; i <= n; i++){
+	for(int i = 0; i < n; i++) {
 		cin >> state;
-		if (state){
-			ans += a[i - 1];
-			dp[i] = max(dp[i - 1] - a[i - 1], -a[i - 1]);
-		} else dp[i] = max(dp[i - 1] + a[i - 1], a[i - 1]);
+		if (state) ans += a[i], a[i] = -a[i];
 	}
-	cout << ans + *max_element(dp + 1, dp + n + 1);
+	int tmp = a[0], end = a[0];
+	for(int i = 1; i < n; i++){
+		end = max(end + a[i], a[i]);
+		tmp = max(tmp, end);
+	}
+	cout << ans + tmp;
 }
 
 //https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
