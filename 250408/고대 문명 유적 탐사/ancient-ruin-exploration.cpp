5f444f59c08ae99c114aa7f2d5b26a71f7c06e85
Sangyun_LEE (이상윤) <falconlee236@cau.ac.kr>
2025-04-08 19:18:22 +0900

    Title: 고대 문명 유적 탐사, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250408/고대 문명 유적 탐사/ancient-ruin-exploration.cpp
+++ 250408/고대 문명 유적 탐사/ancient-ruin-exploration.cpp
@@ -1,26 +1,3 @@
-// https://www.codetree.ai/ko/frequent-problems/problems/ancient-ruin-exploration/description
-/*
-5 * 5, 각 칸에는 1~7까지 숫자 <- 유물조각
-1. 회전축을 아무거나 잡은 다음 그걸 기준으로 90도, 180도, 270도 중 하나 선택해서 회전 (평행이동!)
-
--> 열 부터 확인해야 한다는것이 핵심 (놓치지마라) (왼쪽에서 오른쪽으로, 위에서 아래로)
-
-유물 1차 획득
-그냥 bfs로 같은 숫자끼리 <3개 이상> 이어져 있는 경우
-이때 가치는 모인 조각의 <개수>
-
-유물이 사라지면 벽면에 써있는 숫자대로 조각이 다시 생성
-조건은 (왼쪽에서 오른쪽으로, 아래에서 위로)
-1. 열 번호가 작은 순으로,
-2. 행 번호가 큰 순으로 <- ㅋㅋ 이렇게 낚시를 건다고?
-단 생겨날 조각의 수는 부족하지 않음
-
-한번쓰면 유물조각 다시 사용 불가 이후는 남은 숫자부터 사용
-유물조각이 생성되면 계속 유물을 얻을 수 있고, 더이상 못 얻을때까지 수행
-
-이 과정을 K번 수행함. 출력은 해당 턴에 얻을 수 있는 유물 가치의 총합을 각각 공백으로 출력
-유물을 얻을 수 없으면 출력하지 않고 프로그램 종료
-*/
 #include <iostream>
 #include <cstring>
 #include <queue>
@@ -28,20 +5,6 @@ using namespace std;
 int tmp_arr[5][5], arr[5][5];
 int block[350];
 int block_idx, K, M;
-void print_tmp_arr(){
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 5; j++) cout << tmp_arr[i][j] << " ";
-        cout << "\n";
-    }
-    cout << "-------\n";
-}
-/*
-조건 
-(1) 유물 1차 획득 가치 최대화
-(2) 90도, 180도, 270도 순으로 우선순위 (가장 작은게 높음)
-(3) 회전 중심 좌표의 열이 가장 작음
-(4) 회전 중심 좌표의 행이 가장 작음
-*/
 struct node{
     int x, y, degree, cost;
     bool operator < (const node& n) const {
@@ -51,8 +14,6 @@ struct node{
         return cost < n.cost;
     }
 };
-
-
 void rot_clockwise(int x, int y){
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++) tmp_arr[i + x][j + y] = arr[2 - j + x][i + y];
@@ -124,29 +85,13 @@ int get_benefit(){
             }
         }
         cost += tmp_cost;
-        // 이게 이상함
         for(int i = 0; i < 5; i++){
             for(int j = 4; j >= 0; j--){
                 if (cur_arr[j][i] == 1) tmp_arr[j][i] = block[block_idx++];
             }
         }
-        // cout << "cur_arr \n";
-        // for(int i = 0; i < 5; i++){
-        //     for(int j = 0; j < 5; j++) cout << cur_arr[i][j] << " ";
-        //     cout << "\n";
-        // }
-        // cout << "\n";
-        // cout << "tmp_arr \n";
-        // for(int i = 0; i < 5; i++){
-        //     for(int j = 0; j < 5; j++) cout << tmp_arr[i][j] << " ";
-        //     cout << "\n";
-        // }
-        // cout << "cur block idx = " << block_idx << "\n";
-        // cout << "cur cost = " << cost << "\n";
-        // cout << "-----\n";
     }
     memcpy(arr, tmp_arr, sizeof(arr));
-    // cout << "final cost = " << cost << "\n";
     return cost;
 }
 int main(){
@@ -157,64 +102,22 @@ int main(){
     }
     for(int &x : block) cin >> x;
     priority_queue<node> pq;
-
     while (K--){
-        // 1. 회전축 찾기
         memcpy(tmp_arr, arr, sizeof(arr));
         pq = priority_queue<node>();
         for(int a = 0; a < 3; a++){
             for(int b = 0; b < 3; b++){
-                // cout << "base arr\n";
-                // print_tmp_arr();
-                // 1 - 2. 회전 돌리기 90(시계), 180(반대로), 270(반시계)
                 for(int deg = 90; deg <= 270; deg+=90){
                     rot_clockwise(a, b);
-                    // cout << "cur tmp arr" << deg << "\n";
-                    // print_tmp_arr();
-                    int cost = do_first();
-                    pq.push({a, b, deg, cost});
-                    // cout << a << " " <<  b << " " << cost <<" " << deg << " degree finish\n";
+                    pq.push({a, b, deg, do_first()});
                 }
                 rot_clockwise(a, b);
             }
         }
         auto value = pq.top();
-        // cout << "values \n";
-        // cout << value.x  << " " << value.y << " " << value.cost << value.degree << "\n";
-        
         for(int i = 1; i <= value.degree / 90; i++) rot_clockwise(value.x, value.y);
         int ans = get_benefit();
         if (value.cost == 0) break;
-        // cout << "print ans\n";
         cout << ans << " ";
-        
-        // 2. 조건에 맞춰 회전축 + 방향까지 선택하기
-        // 2 - 1. 유물 찾고 칸 없애기
-        // 2 - 2. 없앤 칸에 유물 채우기 (단 한번 쓴 유물은 게임이 끝날때까지 못씀)
-        // 2 - 3. 2 - 1으로 반복 -> before, after가 같을 때 까지
-        // 3. 모두 0이면 종료
     }
 }
-
-
-/*
-0 0, 0 1, 0 2
-1 0, 1 1, 1 2
-2 0, 2 1, 2 2
-
-시계 (90) 2 - j, i
-2 0, 1 0, 0 0
-2 1, 1 1, 0 1
-2 2, 1 2, 0 2
-
-180도 2 - i, 
-2 2, 2 1, 2 0
-1 2, 1 1, 1 0
-0 2, 0 1, 0 0
-
-270도
-0 2, 1 2, 2 2
-0 1, 1 1, 2 1
-0 0, 1 0, 2 0
-
-*/
